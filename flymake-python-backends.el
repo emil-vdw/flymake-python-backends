fe4da5f57e8939e8489b56ff2ec95862d88ac657
Emil van der Westhuizen <vdwemil@protonmail.com>
2024-09-28 14:40:26 +0200

    Add more backends

--- flymake-python-backends.el
+++ flymake-python-backends.el
@@ -1,116 +1,156 @@
 (require 'f)
 
-(defvar-local flymake-pyton/mypy--proc nil)
+(defvar-local flymake-python/mypy--proc nil)
+(defvar-local flymake-python/flake8--proc nil)
+(defvar-local flymake-python/pylint--proc nil)
 
 (defvar-local flymake-python/mypy--temp-files '())
 
-(defvar flymake-python/mypy-output-regex
+(defvar flymake-python/mypy--output-regex
   "^\\(.*?\\):\\([0-9]+\\):\\([0-9]+\\): \\(.+\\) \\(\\[.+\\]\\)$")
 
-(defun flymake-python-backends/buffer-temp-file-name ()
+(defvar flymake-python/flake8--output-regex
+  "^\\(.*?\\):\\([0-9]+\\):\\([0-9]+\\): \\(\\w+\\) \\(.+\\)$")
+
+(defvar flymake-python/pylint--output-regex
+  "^\\(.*?\\):\\([0-9]+\\):\\([0-9]+\\): \\(\\w+\\): \\(.+\\)$")
+
+(defun flymake-python-backends/buffer-temp-file-name (backend-name)
   ""
   (concat
    "flymake-python-"
+   backend-name
    (substring (secure-hash 'md5 (buffer-name))
               0 16)))
 
-(defun flymake-python-backends/buffer-to-temp-file ()
+(defun flymake-python-backends/buffer-to-temp-file (backend-name)
   ""
   (let ((file-name (f-join (temporary-file-directory)
-                               (flymake-python-backends/buffer-temp-file-name)))
+                               (flymake-python-backends/buffer-temp-file-name
+                                backend-name)))
         (write-region-inhibit-fsync t)
         (jka-compr-inhibit t))
     (make-directory (file-name-directory file-name) t)
     (write-region nil nil file-name nil 0)
     file-name))
 
+
+(defmacro flymake-python-backends/run-backend-for-checker
+    (backend-name
+     executable
+     backend-command
+     proc-var
+     output-parse-fn
+     severity)
+
+  (let ((backend-fn-name (intern (format "flymake-python-backends/%s" backend-name)))
+        (process-name (format "%s-flymake" backend-name))
+        (process-buffer-name (format " *%s-flymake*" backend-name)))
+    `(progn
+       (unless (executable-find ,executable)
+         (error "Cannot find a %s executable" ,executable))
+
+       ;; If a live process launched in an earlier check was found, that process
+       ;; is killed.  When that process's sentinel eventually runs, it will
+       ;; notice its obsoletion since PROC-VAR is set to a different value.
+       (when (process-live-p ,proc-var)
+         (kill-process ,proc-var))
+
+       ;; Save the current buffer, the narrowing restriction, remove any
+       ;; narrowing restriction.
+       (save-restriction
+         (widen)
+         (let ((buffer-temp-file
+                (flymake-python-backends/buffer-to-temp-file ,backend-name)))
+           (setq ,proc-var
+                 (make-process
+                  :name ,process-name
+                  :noquery t
+                  :connection-type 'pipe
+                  :buffer (generate-new-buffer ,process-buffer-name)
+                  :command (list ,@backend-command buffer-temp-file)
+                  :sentinel
+                  (lambda (proc _event)
+                    (when (memq (process-status proc) '(exit signal))
+                      (unwind-protect
+                          (let ((source-buffer (process-get proc 'source-buffer))
+                                (report-fn (process-get proc 'report-fn))
+                                (buffer-diagnostics '()))
+                            (if (with-current-buffer source-buffer (eq proc ,proc-var))
+                                (with-current-buffer (process-buffer proc)
+                                  (goto-char (point-min))
+                                  (while-let ((checker-result (,output-parse-fn)))
+                                    (let* ((region (flymake-diag-region
+                                                    source-buffer
+                                                    (plist-get checker-result :line)
+                                                    (plist-get checker-result :col)))
+                                           (diag (flymake-make-diagnostic
+                                                  source-buffer
+                                                  (car region)
+                                                  (cdr region)
+                                                  ,severity
+                                                  (plist-get checker-result :description))))
+                                      (add-to-list 'buffer-diagnostics diag)))
+
+                                  (kill-buffer (process-buffer proc))
+
+                                  (ignore-errors
+                                    (delete-file buffer-temp-file))
+
+                                  (funcall report-fn buffer-diagnostics))
+
+                              (flymake-log :warning "Canceling obsolete check %s" proc))))))))
+           (process-put ,proc-var 'source-buffer (current-buffer))
+           (process-put ,proc-var 'report-fn report-fn))))))
+
 (defun flymake-python-backends/mypy (report-fn &rest _args)
-  (unless (executable-find "mypy")
-    (error "Cannot find a mypy executable"))
+  (flymake-python-backends/run-backend-for-checker
+   "mypy"
+   "mypy"
+   ("mypy" "--show-column-numbers")
+   flymake-python/mypy--proc
+   (lambda ()
+     (when (search-forward-regexp flymake-python/mypy--output-regex nil t)
+       (let* ((line (string-to-number (match-string 2)))
+              (col (string-to-number (match-string 3)))
+              (type (match-string 5))
+              (message (match-string 4))
+              (description (format "mypy %s: %s" type message)))
+         `(:line ,line :col ,col :type ,type :message ,message :description ,description))))
+   :error))
 
-  ;; If a live process launched in an earlier check was found, that
-  ;; process is killed.  When that process's sentinel eventually runs,
-  ;; it will notice its obsoletion, since it have since reset
-  ;; `mypy-flymake-proc' to a different value
-  ;;
-  (when (process-live-p flymake-pyton/mypy--proc)
-    (kill-process flymake-pyton/mypy--proc))
+(defun flymake-python-backends/flake8 (report-fn &rest _args)
+  (flymake-python-backends/run-backend-for-checker
+   "flake8"
+   "flake8"
+   ("flake8")
+   flymake-python/flake8--proc
+   (lambda ()
+     (when (search-forward-regexp flymake-python/flake8--output-regex nil t)
+       (let* ((line (string-to-number (match-string 2)))
+              (col (string-to-number (match-string 3)))
+              (type (match-string 4))
+              (message (match-string 5))
+              (description (format "flake8 %s: %s" type message)))
+         `(:line ,line :col ,col :type ,type :message ,message :description ,description))))
+   :error))
 
-  ;; Save the current buffer, the narrowing restriction, remove any
-  ;; narrowing restriction.
-  ;;
-  (save-restriction
-    (widen)
-
-    (let ((buffer-temp-file (flymake-python-backends/buffer-to-temp-file)))
-      ;; Reset the `flymake-pyton/mypy--proc' process to a new process
-      ;; calling the mypy tool.
-      ;;
-      (setq
-       flymake-pyton/mypy--proc
-       (make-process
-        :name "mypy-flymake" :noquery t :connection-type 'pipe
-        ;; Make output go to a temporary buffer.
-        ;;
-        :buffer (generate-new-buffer " *mypy-flymake*")
-        :command
-        (list "mypy" buffer-temp-file
-              "--show-column-numbers")
-        :sentinel
-        (lambda (proc _event)
-          ;; Check that the process has indeed exited, as it might
-          ;; be simply suspended.
-          ;;
-          (when (memq (process-status proc) '(exit signal))
-            (unwind-protect
-                ;; Only proceed if `proc' is the same as
-                ;; `flymake-pyton/mypy--proc', which indicates that
-                ;; `proc' is not an obsolete process.
-                ;;
-
-                (let ((source-buffer (process-get proc 'source-buffer))
-                      (report-fn (process-get proc 'report-fn))
-                      (buffer-diagnostics '()))
-
-                  (if (with-current-buffer source-buffer (eq proc flymake-pyton/mypy--proc))
-                      (with-current-buffer (process-buffer proc)
-                        (goto-char (point-min))
-
-                        (while (search-forward-regexp flymake-python/mypy-output-regex (point-max) t)
-                          (let* ((line (string-to-number (match-string 2)))
-                                 (col (string-to-number (match-string 3)))
-                                 (type (match-string 5))
-                                 (msg (match-string 4))
-                                 (description (format "mypy %s: %s" type msg))
-                                 (region (flymake-diag-region
-                                          source-buffer
-                                          line
-                                          col))
-                                 (diag (flymake-make-diagnostic
-                                        source-buffer
-                                        (car region)
-                                        (cdr region)
-                                        :error description)))
-                            (message "%s\n%s\n%s"
-                                     type msg description)
-                            (add-to-list 'buffer-diagnostics diag)))
-
-                        ;; Cleanup the temporary buffer used to hold the
-                        ;; check's output.
-                        ;;
-                        (kill-buffer (process-buffer proc))
-
-                        ;; Clean up the temp file just created.
-                        (ignore-errors
-                          (delete-file buffer-temp-file))
-
-                        (funcall report-fn buffer-diagnostics))
-
-                    (flymake-log :warning "Canceling obsolete check %s"
-                                 proc))))))))
-
-      (process-put flymake-pyton/mypy--proc 'source-buffer (current-buffer))
-      (process-put flymake-pyton/mypy--proc 'report-fn report-fn))))
+(defun flymake-python-backends/pylint (report-fn &rest _args)
+  (flymake-python-backends/run-backend-for-checker
+   "pylint"
+   "pylint"
+   ("pylint" "--disable=C0103")
+   flymake-python/pylint--proc
+   (lambda ()
+     (when (search-forward-regexp flymake-python/pylint--output-regex nil t)
+       (let* ((line (string-to-number (match-string 2)))
+              ;; Pylint columns start at 0.
+              (col (1+ (string-to-number (match-string 3))))
+              (type (match-string 4))
+              (message (match-string 5))
+              (description (format "pylint %s: %s" type message)))
+         `(:line ,line :col ,col :type ,type :message ,message :description ,description))))
+   :error))
 
 (defun ruby-setup-flymake-backend ()
   (add-hook 'flymake-diagnostic-functions 'ruby-flymake nil t))
